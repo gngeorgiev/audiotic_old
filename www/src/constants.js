4f00d9cd3e1159f7ad33eae49076af9b7b3a304c
Georgi Georgiev <gngeorgiev.it@gmail.com>
2016-12-23 21:25:23 +0200

    fixed the portal url to work regardless of where the site/api is hosted

--- www/src/constants.js
+++ www/src/constants.js
@@ -1,2 +1,2 @@
-export const ServerUrl = 'http://localhost:8090';
+export const ServerUrl = `${window.location.host}:8090`;
 export const DefaultThumbnail = 'http://support.yumpu.com/en/wp-content/themes/qaengine/img/default-thumbnail.jpg';
